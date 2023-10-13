a534e53b6819955629062af56dde21202b6e1602
Siubeng <fython@163.com>
2023-10-13 06:28:24 +0000

    finish conversions: from_into/from_str/using_as

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,21 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+// I AM DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let params: Vec<&str> = s.split(",").collect();
+        if params.len() < 2 || params[0].len() == 0 {
+            return Person::default();
+        }
+        params[1]
+            .parse::<usize>()
+            .map(|age| Person {
+                name: params[0].into(),
+                age: age,
+            })
+            .unwrap_or(Person::default())
     }
 }
 
